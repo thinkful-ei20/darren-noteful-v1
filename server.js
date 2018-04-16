a4a9e4a291a59450c727a9ca9d9cb6b7bce9f23d
Darren <ninenumbproductions@gmail.com>
2018-04-16 13:56:25 -0700

    a little more than boilerplate, added get funcitonality. fixing id query

--- server.js
+++ server.js
@@ -5,3 +5,26 @@ const data = require('./db/notes');
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.static('public'));
+
+app.listen(PORT, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const item = data[id];
+  res.json(item);
+});
+
+
