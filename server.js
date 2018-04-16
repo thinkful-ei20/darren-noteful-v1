33a529b637d2a417520d5da57cc621052f6f58f8
Darren <ninenumbproductions@gmail.com>
2018-04-16 13:58:01 -0700

    putlisten at bottom

--- server.js
+++ server.js
@@ -11,12 +11,6 @@ const PORT = 8080;
 
 app.use(express.static('public'));
 
-app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
-
 app.get('/api/notes', (req, res) => {
   res.json(data);
 });
@@ -25,6 +19,17 @@ app.get('/api/notes/:id', (req, res) => {
   const id = req.params.id;
   const item = data[id];
   res.json(item);
+});
+
+
+
+
+
+
+app.listen(PORT, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
 });
 
 
