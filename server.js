152eb1cd6da857be40d1b8fbc4fc3ad0063d6d29
Darren <ninenumbproductions@gmail.com>
2018-04-17 14:25:51 -0700

    halfway through assignment. queries added. moving onto add simDB to app

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ console.log('Hello Noteful!');
 // INSERT EXPRESS APP CODE HERE...
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const {PORT} = require('./config');
+const {logger} = require('./middleware/logger');
+
+
+app.use(logger);
 
 app.use(express.static('public'));
+
 
 app.get('/api/notes', (req, res) => {
 
@@ -23,16 +28,38 @@ app.get('/api/notes', (req, res) => {
   } else {
     res.json(data);
   }
-
-  // (searchTerm) ? data.filter(item =)
+ 
 });
 
-app.get('/api/notes/:id', (req, res) => {
+
+app.get('/api/notes/:id', (req, res, next) => {
   const id = +req.params.id;    
   const foundItem = data.find(item => item.id === id);
-  res.json(foundItem);
+
+  if(foundItem) {
+    res.json(foundItem);
+  } else {
+    next();
+  }  
 });
 
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boomshakalaka!!');
+});
+
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
 
 app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
