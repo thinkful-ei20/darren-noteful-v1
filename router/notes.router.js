bf341be3fb2c86f2907c8962f98b40b38fdaf4fb
Darren <ninenumbproductions@gmail.com>
2018-04-18 13:13:42 -0700

    Post set up

--- router/notes.router.js
+++ router/notes.router.js
@@ -39,10 +39,6 @@ router.get('/notes/:id', (req, res, next) => {
   });
 });
 
-
-
-
-
 router.put('/notes/:id', (req, res, next) => {
   const id = req.params.id;
 
@@ -74,5 +70,31 @@ router.put('/notes/:id', (req, res, next) => {
     }
   });
 });
+
+// Post (insert) an item
+router.post('/notes', (req, res, next) => {
+  const { title, content } = req.body;
+
+  const newItem = { title, content };
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+
 
 module.exports = router;
